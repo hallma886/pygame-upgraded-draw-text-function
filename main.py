11ece7bdd62ae8cef3373a64569429a195fcdd6e
hallma886 <hallma886@northwested.org>
2025-03-11 13:07:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,26 @@ def main():
     screen = init_game()  # Initialize the game and get the screen
     clock = pygame.time.Clock() # Initialize the clock objecct
     
+    # Define font properties
+    font_name = "Caveat-VariableFont_wght.ttf"
+    font_color1 = config.RED    # Use a color from config
+    font_color2 = config.BLUE
+    font_color3 = config.GREEN
+    font_size_normal = 36
+    font_size_bold_italic = 30
+    font_size_custom = 48
+
+    # Using a True Type Font (.ttf)
+    custom_font_name = "Caveat-VariableFont_wght.ttf"
+
+    # Define x,y coordinate pairs for top left of text rectangle (stamp)
+    text_position_1 = [50, 50]
+    text_position_2 = [225, 135]
+    text_position_3 = [220, 370]
+
+    def draw_text
+
+
 
  
     # Main game loop
